3fd8d60fd5b7ca872be96cf291de7b3fb65b47f2
Lovika Gosain <32248818+lg579@users.noreply.github.com>
2017-10-28 06:59:11 -0400

    final.html

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import flask
 from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-   return render_template('orgsDescriptions.html')
-@app.route('/OrgsSummary')
-def OrgsSummarymethod():
-    return render_template('OrgsSummary.html')
+   return render_template('final.html')
+
 if __name__ == "__main__":
 	app.run()
 	

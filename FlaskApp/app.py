158fb38e55e5f101fae6ab02c8de793d1693e30d
kushbaldha <kush.baldha@gmail.com>
2017-10-28 05:46:00 -0400

    orgs pages are up

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-
-
 
 
 
 @app.route("/")
 def main():
-	return "Welcome this is amazing!"
-
+   return render_template('orgsDescriptions.html')
+@app.route('/OrgsSummary')
+def OrgsSummarymethod():
+    return render_template('OrgsSummary.html')
 if __name__ == "__main__":
-    	app.run()
+	app.run()
+	
